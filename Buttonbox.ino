09e3b2c15e49e934d29db6864ff65bc750b2523e
AlBundy33 <6294586+AlBundy33@users.noreply.github.com>
2020-03-21 16:34:54 +0100

    some minor chages

--- Buttonbox.ino
+++ Buttonbox.ino
@@ -6,19 +6,23 @@
 // comment this line if you want to simulate a keyboard instead of a joystick
 #define ENABLE_JOYSTICK
 
-// library to handle the matrix
+// Keypad by Alexander Brevig, Mark Stanley
+// https://playground.arduino.cc/Code/Keypad/
 #include <Keypad.h>
-// library to work with rotary encoders
+// RotaryEncoder by Matthias Hertel
+// https://github.com/mathertel/RotaryEncoder
 #include <RotaryEncoder.h>
 #ifdef ENABLE_JOYSTICK
+// https://github.com/MHeironimus/ArduinoJoystickLibrary
 #include <Joystick.h>
 #else
 #include <Keyboard.h>
 #endif
 
-// define the matrix pins
+// define the matrix rows and colums
 #define MATRIX_ROWS 3
 #define MATRIX_COLUMNS 4
+// define the matrix pins
 byte rowPins[MATRIX_ROWS] = {20, 19, 18};
 byte colPins[MATRIX_COLUMNS] = {15, 14, 16, 10};
 
@@ -30,13 +34,13 @@ byte matrix[MATRIX_ROWS][MATRIX_COLUMNS] = {
   { 8, 9,10,11},
 };
 
-// define the number of used buttons in our matrix
-#define PHYSICAL_BUTTONS 12
+// define the number of used buttons in our matrix (by default all buttons are used)
+#define PHYSICAL_BUTTONS MATRIX_ROWS * MATRIX_COLUMNS
 
 // now define the rotaries (values are the used pins to read the data)
 #define ROTARIES 4
 RotaryEncoder rotaries[ROTARIES] = {
-  {1,0},
+  {1,0}, // I had to switch the pins because I solderd them wrong :-)
   {2,3},
   {4,5},
   {6,7}
@@ -59,7 +63,7 @@ char KEYBOARD_KEYS[VIRTUAL_BUTTONS + (ROTARIES * 2)] = {
 
 bool button_hold_states[VIRTUAL_BUTTONS];
 
-Keypad button_box = Keypad(makeKeymap(matrix), rowPins, colPins, sizeof(rowPins), sizeof(colPins));
+Keypad button_box = Keypad(makeKeymap(matrix), rowPins, colPins, MATRIX_ROWS, MATRIX_COLUMNS);
 
 #ifdef ENABLE_JOYSTICK
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID,
@@ -85,62 +89,57 @@ void loop()
   CheckAllButtons();
 }
 
-void CheckAllButtons(void) {
-  if (button_box.getKeys())
+void CheckAllButtons() {
+  if (!button_box.getKeys())
+    return;
+
+  for (int i = 0; i < LIST_MAX; i++)
   {
-    for (int i = 0; i < LIST_MAX; i++)
+    if ( !button_box.key[i].stateChanged )
+      continue;
+
+    switch (button_box.key[i].kstate)
     {
-      if ( button_box.key[i].stateChanged )
-      {
-        switch (button_box.key[i].kstate)
-        {
-          #ifdef ENABLE_DOUBLE_FUNCTION
-            case PRESSED:
-              break;
-            case HOLD:
-              button_hold_states[button_box.key[i].kcode] = true;
-              pressButton(button_box.key[i].kcode + PHYSICAL_BUTTONS, true); // long press
-              break;
-            case RELEASED:
-                if (button_hold_states[button_box.key[i].kcode])
-                  pressButton(button_box.key[i].kcode + PHYSICAL_BUTTONS, false); // end long press  
-                else
-                  pressButton(button_box.key[i].kcode); // short press
-              break;
-            case IDLE:
-              button_hold_states[button_box.key[i].kcode] = false;
-              break;
-          #else
-            case PRESSED:
-            case HOLD:
-              pressButton(button_box.key[i].kchar, true);
-              break;
-            case RELEASED:
-            case IDLE:
-              pressButton(button_box.key[i].kchar, false);
-              break;        
-          #endif
-        }
-      }
+      #ifdef ENABLE_DOUBLE_FUNCTION
+        case PRESSED:
+          break;
+        case HOLD:
+          button_hold_states[button_box.key[i].kcode] = true;
+          pressButton(button_box.key[i].kcode + PHYSICAL_BUTTONS, true); // long press
+          break;
+        case RELEASED:
+            if (button_hold_states[button_box.key[i].kcode])
+              pressButton(button_box.key[i].kcode + PHYSICAL_BUTTONS, false); // end long press  
+            else
+              pressButton(button_box.key[i].kcode); // short press
+          break;
+        case IDLE:
+          button_hold_states[button_box.key[i].kcode] = false;
+          break;
+      #else
+        case PRESSED:
+        case HOLD:
+          pressButton(button_box.key[i].kchar, true);
+          break;
+        case RELEASED:
+        case IDLE:
+          pressButton(button_box.key[i].kchar, false);
+          break;        
+      #endif
     }
   }
 }
 
-
-void CheckAllEncoders(void)
+void CheckAllEncoders()
 {
   for (int i = 0; i < ROTARIES; i++)
   {
     rotaries[i].tick();
     RotaryEncoder::Direction d = rotaries[i].getDirection();
     if (d == RotaryEncoder::Direction::COUNTERCLOCKWISE)
-    {
       pressButton(VIRTUAL_BUTTONS + (i*2));
-    }
     else if (d == RotaryEncoder::Direction::CLOCKWISE)
-    {
       pressButton(VIRTUAL_BUTTONS + (i*2) + 1);
-    };
   }
 }
 
